e329403d53c57f51ef5c0479f92078fccdae30a0
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-05 22:57:57 -0300

    Feat: Adiciona service da proposta, que sera responsavel por conter as regras de negocio da aplicacao

---
+++ src/main/java/com/dev/propostaapp/service/PropostaSerivce.java
@@ -0,0 +1,13 @@
+package com.dev.propostaapp.service;
+
+import com.dev.propostaapp.dto.PropostaRequestDTO;
+import com.dev.propostaapp.dto.PropostaResponseDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropostaSerivce {
+
+    public PropostaResponseDTO criar(PropostaRequestDTO propostaRequestDTO) {
+        return null;
+    }
+}
