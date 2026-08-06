f988aceb88a88371e7c55d8315f8082a331d822f
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-06 17:49:57 -0300

    Feat: Adiciona mapeamento de entity para dto

--- src/main/java/com/dev/propostaapp/mapper/PropostaMapper.java
+++ src/main/java/com/dev/propostaapp/mapper/PropostaMapper.java
@@ -1,12 +1,16 @@
 package com.dev.propostaapp.mapper;
 
 import com.dev.propostaapp.dto.PropostaRequestDTO;
+import com.dev.propostaapp.dto.PropostaResponseDTO;
 import com.dev.propostaapp.entity.Proposta;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface PropostaMapper {
+
+    PropostaMapper INSTANCE = Mappers.getMapper(PropostaMapper.class);
 
     @Mapping(target = "usuario.nome", source = "nome")
     @Mapping(target = "usuario.sobrenome", source = "sobrenome")
@@ -18,4 +22,11 @@ public interface PropostaMapper {
     @Mapping(target = "integrada", ignore = true)
     @Mapping(target = "observacao", ignore = true)
     Proposta convertDtoToProposta(PropostaRequestDTO propostaRequestDTO);
+
+    @Mapping(target = "nome", source = "usuario.nome")
+    @Mapping(target = "sobrenome", source = "usuario.sobrenome")
+    @Mapping(target = "telefone", source = "usuario.telefone")
+    @Mapping(target = "cpf", source = "usuario.cpf")
+    @Mapping(target = "renda", source = "usuario.renda")
+    PropostaResponseDTO convertEntityToDto(Proposta proposta);
 }
