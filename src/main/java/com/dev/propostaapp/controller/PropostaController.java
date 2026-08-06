70ee49edace11e2a073561f64f7708da9663f617
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-05 22:58:34 -0300

    Feat: Injeta dependencia do service no controller e utiliza metodo do service de criar no endpoint de criar proposta

--- src/main/java/com/dev/propostaapp/controller/PropostaController.java
+++ src/main/java/com/dev/propostaapp/controller/PropostaController.java
@@ -2,6 +2,8 @@ package com.dev.propostaapp.controller;
 
 import com.dev.propostaapp.dto.PropostaRequestDTO;
 import com.dev.propostaapp.dto.PropostaResponseDTO;
+import com.dev.propostaapp.service.PropostaSerivce;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/proposta")
 public class PropostaController {
 
+    private PropostaSerivce propostaSerivce;
+
+    public PropostaController(PropostaSerivce propostaSerivce) {
+        this.propostaSerivce = propostaSerivce;
+    }
+
     @PostMapping
     public ResponseEntity<PropostaResponseDTO> criar(@RequestBody PropostaRequestDTO propostaRequestDTO) {
-        return null;
+        PropostaResponseDTO propostaResponseDTO = propostaSerivce.criar(propostaRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(propostaResponseDTO);
     }
 
 
