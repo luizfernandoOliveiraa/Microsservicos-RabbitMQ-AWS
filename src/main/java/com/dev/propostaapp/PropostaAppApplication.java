d2581fce01c767e02e6bd236610a88f360fdf46a
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-05 21:10:02 -0300

    Config: Adiciona primeiros arquivos de config na aplicacao

--- src/main/java/com/dev/propostaapp/PropostaAppApplication.java
+++ src/main/java/com/dev/propostaapp/PropostaAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PropostaAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PropostaAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PropostaAppApplication.class, args);
+    }
 
 }
