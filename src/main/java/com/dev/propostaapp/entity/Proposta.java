ac461101611936b873c73065fd562201dca4e2be
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-06 18:51:28 -0300

    Feat: Adiciona anotacao de Cascade do jakarta, permitindo criar 2 entidades a partir de 1 so

--- src/main/java/com/dev/propostaapp/entity/Proposta.java
+++ src/main/java/com/dev/propostaapp/entity/Proposta.java
@@ -1,12 +1,6 @@
 package com.dev.propostaapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -32,7 +26,7 @@ public class Proposta {
 
     private String observacao;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
 
