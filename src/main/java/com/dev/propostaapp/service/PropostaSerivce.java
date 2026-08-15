a28cfed2e160b9719ada28103ed7a155dccb6435
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-14 21:08:32 -0300

    Feat: adiciona logica no service para envio de mensagem a partir da criacao da proposta

--- src/main/java/com/dev/propostaapp/service/PropostaSerivce.java
+++ src/main/java/com/dev/propostaapp/service/PropostaSerivce.java
@@ -5,23 +5,30 @@ import com.dev.propostaapp.dto.PropostaResponseDTO;
 import com.dev.propostaapp.entity.Proposta;
 import com.dev.propostaapp.mapper.PropostaMapper;
 import com.dev.propostaapp.repository.PropostaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class PropostaSerivce {
 
-    private PropostaRepository propostaRepository;
+    private final PropostaRepository propostaRepository;
+    private final NotificationService notificationService;
 
-    public PropostaSerivce(PropostaRepository propostaRepository) {
-        this.propostaRepository = propostaRepository;
-    };
+    @Value("${rabbitmq.propostapendente.exchange}")
+    private String exchange;
 
     public PropostaResponseDTO criar(PropostaRequestDTO propostaRequestDTO) {
         Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(propostaRequestDTO);
         propostaRepository.save(proposta);
-        return PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+
+        PropostaResponseDTO response = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+        notificationService.notificar(response, exchange);
+
+        return response;
     }
 
     public List<PropostaResponseDTO> obterProposta() {
