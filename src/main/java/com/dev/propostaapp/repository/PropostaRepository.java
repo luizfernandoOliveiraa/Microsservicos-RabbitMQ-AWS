a02515da9b6a65366df57d199f8741ea481f2029
luizfernando <lf.nandooliveeira@gmail.com>
2026-08-15 21:17:35 -0300

    Feat: Adiciona metodos para encontrar propostas nao integradas e atualizacao de proposta

--- src/main/java/com/dev/propostaapp/repository/PropostaRepository.java
+++ src/main/java/com/dev/propostaapp/repository/PropostaRepository.java
@@ -1,11 +1,22 @@
 package com.dev.propostaapp.repository;
 
 import com.dev.propostaapp.entity.Proposta;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
 
-    
+    List<Proposta> findAllByIntegradaIsFalse();
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE proposta SET aprovada = :aprovada, observacao = :observacao WHERE id = :id", nativeQuery = true)
+    void atualizarProposta(Long id, boolean aprovada, String observacao);
+
 }
